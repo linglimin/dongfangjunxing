cf23f61288bd13ec3d8f899ae3737e9b756ed574
liwanhe <liwanhe@01zhuanche.com>
2019-06-25 01:07:24 +0800

    head

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,11 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    navbarData:{
+        showCapsule:1,
+        title:'首页'
+    }
   },
   //事件处理函数
   bindViewTap: function() {
